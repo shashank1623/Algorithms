fa21b65b9f2ac7fc0dd90880ed13b186bc0083ef
shashank2309 <116372555+shashank2309@users.noreply.github.com>
2022-10-21 21:06:37 +0530

    Create Longest Substring Without Repeating Characters.cpp

---
+++ Popular Coding Topics/Greedy Algorithms/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,26 @@
+/* 
+
+problem statement:-
+Given a string s, find the length of the longest substring without repeating characters.
+
+solution:-
+
+*/
+
+
+class Solution {
+public:
+int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+
+// problem link:- https://leetcode.com/problems/longest-substring-without-repeating-characters/
